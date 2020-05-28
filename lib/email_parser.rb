f38bac1ac341e82b8fd7841cc9d397b14d255577
Thanh Pham <tpham0123@gmail.com>
2020-05-28 01:48:52 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -28,9 +28,9 @@ class EmailAddressParser
   end 
   
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+    email_addresses = @email.split(/[, ]/).uniq
+    email_addresses.reject! {|element| element.empty?}
+    email_addresses
   end
    
   
